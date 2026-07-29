258b716d07fa7b800abbc13716df3cb7fac06ad7
VARUN D <varun030406@gmail.com>
2026-07-29 08:11:07 +0530

    Update 10phpDate&prime.php

--- 10phpDate&prime.php
+++ 10phpDate&prime.php
@@ -18,45 +18,40 @@ echo "Today's Date: " . date("d-m-Y");
 //prime
 <!DOCTYPE html>
 <html>
-
 <head>
-  <title>Prime Number Checker</title>
+    <title>Prime Number Check</title>
 </head>
-
 <body>
-  <h1>Prime Number Checker</h1>
-  <form method="POST">
-    <label>Number</label>
-    <input name="num" type="number" required />
-    <button type="submit">Check!</button>
-  </form>
-  <p>
-    <?php
-    function isPrime($num)
-    {
-      if ($num <= 1)
-        return false;
-      if ($num == 2)
-        return true;
-      for ($i = 2; $i <= sqrt($num); $i++) {
-        if ($num % $i == 0)
-          return false;
-      }
-      return true;
-    }
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-      $num = $_POST["num"];
-      if ($num == 1 || $num == 0) {
-        echo "The number $num is neither prime nor composite";
-      } else if (isPrime($num)) {
-        echo "The number $num is a prime number";
-      } else {
-        echo "The number $num is not a prime number";
-      }
+<form method="post">
+    Enter a Number:
+    <input type="number" name="num" required>
+    <input type="submit" value="Check">
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $num = $_POST["num"];
+
+    if ($num <= 1) {
+        echo "$num is not a Prime Number.";
+    } else {
+        $prime = true;
+
+        for ($i = 2; $i <= sqrt($num); $i++) {
+            if ($num % $i == 0) {
+                $prime = false;
+                break;
+            }
+        }
+
+        if ($prime)
+            echo "$num is a Prime Number.";
+        else
+            echo "$num is not a Prime Number.";
     }
-    ?>
-  </p>
+}
+?>
+
 </body>
-
 </html>
